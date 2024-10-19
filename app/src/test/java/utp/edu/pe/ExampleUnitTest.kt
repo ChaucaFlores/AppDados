7370c1f2556c90c4f0ee5a01cd1076a09f27ca7b
Albert Silvera <147192329+Albertsilveraa@users.noreply.github.com>
2024-10-18 20:21:59 -0500

    Actualizar ExampleUnitTest.kt

--- app/src/test/java/utp/edu/pe/ExampleUnitTest.kt
+++ app/src/test/java/utp/edu/pe/ExampleUnitTest.kt
@@ -15,5 +15,8 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 }
-* See [testing documentation](http://d.android.com/tools/testing).
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
  */
