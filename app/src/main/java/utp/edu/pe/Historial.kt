c3506555fb48e88513c682d8bcd38165d158e452
ChaucaFlores <chaucafloresjesusmiguel@gmail.com>
2024-11-22 13:57:04 -0500

    Update Historial.kt
    Detalles de la clase

--- app/src/main/java/utp/edu/pe/Historial.kt
+++ app/src/main/java/utp/edu/pe/Historial.kt
@@ -18,13 +18,11 @@ class Historial : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historial)
 
-        // Obtener la referencia del TableLayout
         historialContent = findViewById(R.id.historial_content)
 
-        // Recuperar la lista de resultados enviada desde Lanzamiento
         val historialResultados = intent.getStringArrayListExtra("historial") ?: arrayListOf()
-        val tipoDado = intent.getIntExtra("tipo_dado", 6) // Recuperar el tipo de dado
-        val cantidadDados = intent.getIntExtra("cantidad_dados", 1) // Recuperar la cantidad de dados
+        val tipoDado = intent.getIntExtra("tipo_dado", 6) 
+        val cantidadDados = intent.getIntExtra("cantidad_dados", 1) 
 
         val tableRowHeader = TableRow(this)
         val textViewHeader = TextView(this)
@@ -33,25 +31,19 @@ class Historial : AppCompatActivity() {
         textViewHeader.textSize = 30f
         textViewHeader.setTypeface(null, android.graphics.Typeface.BOLD)
         textViewHeader.setPadding(8, 8, 8, 8)
-        textViewHeader.setTextColor(resources.getColor(android.R.color.black))  // Cambiar color del encabezado a negro
+        textViewHeader.setTextColor(resources.getColor(android.R.color.black))  
         tableRowHeader.addView(textViewHeader)
         historialContent.addView(tableRowHeader)
 
-        // Verifica si la lista tiene resultados y muestra
+        
         if (historialResultados.isNotEmpty()) {
             for (resultado in historialResultados) {
                 val tableRow = TableRow(this)
-
-                // Crear un TextView para cada resultado
                 val textView = TextView(this)
                 textView.text = resultado
                 textView.setPadding(38, 0, 0, 0)
                 textView.setTextColor(resources.getColor(android.R.color.black))
-
-                // Añadir el TextView a la fila
                 tableRow.addView(textView)
-
-                // Añadir la fila al TableLayout
                 historialContent.addView(tableRow)
             }
         } else {
@@ -59,25 +51,21 @@ class Historial : AppCompatActivity() {
             val textView = TextView(this)
             textView.text = "No hay resultados aún."
             textView.setPadding(8, 8, 8, 8)
-            textView.setTextColor(resources.getColor(android.R.color.black))  // Cambiar color del texto a negro
+            textView.setTextColor(resources.getColor(android.R.color.black))  
             tableRow.addView(textView)
             historialContent.addView(tableRow)
         }
-
-        // Configurar los botones
+        
         btnVolverLanzar = findViewById(R.id.button1)
         btnMenu = findViewById(R.id.button2)
-
         btnVolverLanzar.setOnClickListener {
-            // Volver a la actividad de lanzamiento
             onBackPressed()
         }
 
         btnMenu.setOnClickListener {
-            // Crear un Intent para ir a la actividad Menu
             val intent = Intent(this, Menu::class.java)
-            startActivity(intent)  // Iniciar la actividad Menu
-            finish()  // Opcional, si quieres cerrar la actividad Historial después de abrir Menu
+            startActivity(intent)  
+            finish()  
         }
     }
 }
